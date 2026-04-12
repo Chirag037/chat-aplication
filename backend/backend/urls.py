597badd020f85e639b00f6a9b597c551d688b0b9
chirag chapagain <chiragchapagain03@gmail.com>
2026-04-12 11:53:26 +0545

    updating

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,6 +7,7 @@ from chat.views import health_check, register, login, messages, list_users, room
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('api/health/', health_check),
     path('api/register/', register),
     path('api/login/', login),
@@ -16,11 +17,13 @@ urlpatterns = [
     path('api/rooms/direct/', create_direct_room),
     path('api/rooms/group/', list_group_rooms),
     path('api/rooms/join/', join_group_room),
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('<path:rest>', TemplateView.as_view(template_name='index.html')),
+    
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-else:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    re_path(r'^.*/$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+]
